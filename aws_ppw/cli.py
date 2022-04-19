b8e02935af57756dc93f449e118d3f2dd090988f
Joshua Russo <josh.r.russo@gmail.com>
2022-04-19 17:22:17 -0400

    reverted cli

--- aws_ppw/cli.py
+++ aws_ppw/cli.py
@@ -3,18 +3,5 @@ import sys
 
 def main():
     cwd = os.path.dirname(__file__)
-    package_dir = None
-
-    if sys.argv[1] == "in-place":
-        # Skip the project folder
-        package_dir = os.path.abspath(os.path.join(cwd, "../{{cookiecutter.project_slug}}/"))
-
-    elif len(sys.argv) > 1:        
-        print("Unknown argument(s)")
-        print(sys.argv[1:])
-
-    else:
-        package_dir = os.path.abspath(os.path.join(cwd, "../"))
-
-    if package_dir:
-        os.system(f"cookiecutter {package_dir}")
+    package_dir = os.path.abspath(os.path.join(cwd, "../"))
+    os.system(f"cookiecutter {package_dir}")
