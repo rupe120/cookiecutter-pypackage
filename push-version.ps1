03cde4915114486782acfe42936478e4b46a6dd8
Joshua Russo <josh.r.russo@gmail.com>
2022-04-19 17:03:10 -0400

    v1.1.11

--- push-version.ps1
+++ push-version.ps1
@@ -1,11 +1,12 @@
-param{
-    [Parameter()]
-    [string]$version
-    [string]$description
-}
+param(
+    [Parameter(Mandatory)]
+    [string]$version,
+    [Parameter(Mandatory)]
+    [string]$message
+)
 
 poetry version $version
 git add -A
 git commit -m "v$version"
-git tag v$version -m $description
+git tag v$version -m $message
 git push --atomic origin master v$version
