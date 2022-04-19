1a66a1d69acea7b772b02a3026f81ea0a86dc91f
Joshua Russo <josh.r.russo@gmail.com>
2022-04-19 16:51:59 -0400

    updated cli logic

--- aws_ppw/cli.py
+++ aws_ppw/cli.py
@@ -4,13 +4,17 @@ import sys
 def main():
     cwd = os.path.dirname(__file__)
     package_dir = None
+
     if sys.argv[0] == "in-place":
-        package_dir = os.path.abspath(os.path.join(cwd, "../"))
-    elif len(sys.argv) > 0:
-        print("Invalid argument(s)")
-        print(sys.argv)
-    else:
+        # Skip the project folder
         package_dir = os.path.abspath(os.path.join(cwd, "../{{cookiecutter.project_slug}}/"))
+
+    elif len(sys.argv) > 1:        
+        print("Invalid argument(s)")
+        print(sys.argv[1:])
+
+    else:
+        package_dir = os.path.abspath(os.path.join(cwd, "../"))
 
     if package_dir:
         os.system(f"cookiecutter {package_dir}")
